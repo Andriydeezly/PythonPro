e47907b9a60efb42b0b0f1de4b1b2f71e57700cf
Andriy S <bkskarovsky@gmail.com>
2023-03-05 01:52:04 +0200

    get fuel value

--- fuel_value.py
+++ fuel_value.py
@@ -1,9 +1,40 @@
-"Росхід палива на 100 км"
+# "Росхід палива на 100 км"
+#
+# import tkinter as tk
+#
+# win = tk.Tk()
+# win.geometry(f'950x500+100+200')
+# win['bg'] = '#33ffe6'
+# win.title('Шпіліанський росхід бензи')
+#
+# label_probig = tk.Label(win, text ="Пробіг (км)", font=('Arial', 15))
+# label_probig.pack()
+#
+#
+# win.mainloop()
 
-import tkinter as tk
+import django
+import requests as rec
+import enum
 
-win = tk.Tk()
-win.geometry(f'250x300+100+200')
-win['bg'] = '#33ffe6'
-win.title('Шпіліанський росхід бензи')
+class FuelType(enum.Enum):
+    A95 = 'a95f'
+    A95P = 'a95pf'
+    A92 = 'a92f'
+    Diesel = 'dtf'
+
+def Milage(fuelPrice, distance, fuelPerKilometr):
+
+    fuelLiters = distance/ fuelPerKilometr
+    totalPrice = fuelLiters * fuelPrice
+    return totalPrice
+
+def getFuelPrice(fuelType):
+    link = "https://auto.ria.com/content/news/templetify/fuel_price_page"
+    data = rec.get(link).json()
+    fuelList = data['buckets']
+    for item in fuelList:
+        return item[fuelType.value]['avg']
+
+
 
